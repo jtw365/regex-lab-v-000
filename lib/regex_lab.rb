0f71b9b46f37defdcffa76797e30180421b06453
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-27 11:24:37 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 def starts_with_a_vowel?(word)
   word.match(/\b[aeiou]/i) ? true : false
-# binding.pry
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan()
 
 end
 
