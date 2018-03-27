752172291bac6d9a4b774324af15b92231845af0
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-27 11:21:15 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.match(/\b[aeiou]/)
+# binding.pry
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
